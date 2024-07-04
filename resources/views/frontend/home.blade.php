ef04db24233ef454423874a97712002be738e0f1
sulaksana234 <sulaksana60@gmail.com>
2024-07-04 08:56:54 +0800

    update'

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -78,14 +78,13 @@
                     </div>
                     <div class="ms-auto ps-3 align-self-center text-center">
                         <p class="color-highlight font-10 mb-n2">per biji</p>
-                        <h6 class="font-15 mb-0">Rp. {{ $biji->harga }},- &nbsp;</h6>
-                        <div class="mx-auto">
-                        </div>
+                        <h6 class="font-15 mb-0">Rp. <span id="hargaBiji">{{ $biji->harga }}</span>,- &nbsp;</h6>
+
                     </div>
                     <div class="stepper rounded-xs float-end">
-                        <a href="#" class="stepper-sub"><i class="fa fa-minus color-red-dark"></i></a>
-                        <input type="number" min="0" max="99" value="0">
-                        <a href="#" class="stepper-add"><i class="fa fa-plus color-green-dark"></i></a>
+                        <a href="#" class="stepper-sub" onclick="kurangBijiPentol()"><i class="fa fa-minus color-red-dark"></i></a>
+                        <input type="number" min="0" max="{{ $biji->stok }}" value="0" id="bijiQty" name="bijiQty">
+                        <a href="#" class="stepper-add" onclick="tambahBijiPentol()"><i class="fa fa-plus color-green-dark"></i></a>
                     </div>
                 </div>
                 @endforeach
@@ -102,14 +101,13 @@
                     </div>
                     <div class="ms-auto ps-3 align-self-center text-center">
                         <p class="color-highlight font-10 mb-n2">per gelas</p>
-                        <h6 class="font-15 mb-0">Rp. {{ $gelas->harga }},- &nbsp;</h6>
-                        <div class="mx-auto">
-                        </div>
+                        <h6 class="font-15 mb-0">Rp. <span id="hargaGelas">{{ $gelas->harga }}</span>,- &nbsp;</h6>
+
                     </div>
                     <div class="stepper rounded-xs float-end">
-                        <a href="#" class="stepper-sub"><i class="fa fa-minus color-red-dark"></i></a>
-                        <input type="number" min="0" max="99" value="0">
-                        <a href="#" class="stepper-add"><i class="fa fa-plus color-green-dark"></i></a>
+                        <a href="#" class="stepper-sub" onclick="kurangGelas(event)"><i class="fa fa-minus color-red-dark"></i></a>
+                        <input type="number" min="0" max="{{ $gelas->stok }}" value="0" id="gelasQty" name="gelasQty">
+                        <a href="#" class="stepper-add" onclick="tambahGelas(event)"><i class="fa fa-plus color-green-dark"></i></a>
                     </div>
                 </div>
                 @endforeach
@@ -127,14 +125,13 @@
                     </div>
                     <div class="ms-auto ps-3 align-self-center text-center">
                         <p class="color-highlight font-10 mb-n2">per bungkus</p>
-                        <h6 class="font-15 mb-0">Rp. {{ $bungkus->harga }},- &nbsp;</h6>
-                        <div class="mx-auto">
-                        </div>
+                        <h6 class="font-15 mb-0">Rp. <span id="hargaSnack">{{ $bungkus->harga }}</span>,- &nbsp;</h6>
+
                     </div>
                     <div class="stepper rounded-xs float-end">
-                        <a href="#" class="stepper-sub"><i class="fa fa-minus color-red-dark"></i></a>
-                        <input type="number" min="0" max="99" value="0">
-                        <a href="#" class="stepper-add"><i class="fa fa-plus color-green-dark"></i></a>
+                        <a href="#" class="stepper-sub" onclick="kurangSnack(event)"><i class="fa fa-minus color-red-dark"></i></a>
+                        <input type="number" min="0" max="{{ $bungkus->stok }}" value="0" id="snackQty" name="snackQty">
+                        <a href="#" class="stepper-add" onclick="tambahSnack(event)"><i class="fa fa-plus color-green-dark"></i></a>
                     </div>
                 </div>
                 @endforeach
@@ -143,44 +140,164 @@
         <div class="card card-style">
             <div class="content mt-3">
                 <div class="row mb-0">
-    
+
                     <div class="col-6">
-                        <h4 class="opacity-50 font-13">Items &nbsp;<span class="badge bg-transparent border border-red-dark color-red-dark ms-2"> 5 </span></h4>
+                        <h4 class="opacity-50 font-13">Total &nbsp;<span class="badge bg-transparent border border-red-dark color-red-dark ms-2"> 5 </span></h4>
                     </div>
                     <div class="col-6 text-end">
-                        <h4 class="font-13 opacity-50">$45.10</h4>
+                        <h4 class="font-13 opacity-50" id="totalHarga">0</h4>
                     </div>
                     <div class="col-6">
                         <h4 class="opacity-50 font-13">Diskon &nbsp;<span class="badge bg-transparent border border-red-dark color-red-dark ms-2">0 %</span></h4>
                     </div>
                     <div class="col-6 text-end">
-                        <h4 class="font-13 opacity-50">$15.02</h4>
+                        <h4 class="font-13 opacity-50">Rp.0</h4>
                     </div>
-                    <div class="col-6">
-                        <h4 class="opacity-50 font-13">Delivery &nbsp;<span class="badge bg-transparent border border-red-dark color-red-dark ms-2">Free</span></h4>
-                    </div>
-                    <div class="col-6 text-end">
-                        <h4 class="font-13 opacity-50">$15.02</h4>
-                    </div>
+
                     <div class="col-12 pb-3"></div>
                     <div class="col-6">
-                        <h4>Total</h4>
+                        <h4>Sub Total</h4>
                     </div>
                     <div class="col-6 text-end">
-                        <h4>{{ $biji->harga +  $gelas->harga +  $bungkus->harga}}</h4>
+                        <h4 class="font-15 mb-0"><span id="subTotalHarga">0</span>,-</h4>
                     </div>
-    
+
                 </div>
-    
+
             </div>
-    
+
             <a href="#"
                 class="btn btn-full me-3 ms-3 btn-l font-13 font-600 rounded-s shadow-l mb-4 gradient-highlight"><i
                     class="fas fa-cart-plus"></i>&nbsp;Place Order</a>
-    
+
         </div>
 
     </div>
-    
+
 </div>
+<script>
+        const bijiInput = document.getElementById('bijiQty');
+        const gelasInput = document.getElementById('gelasQty');
+        const snackInput = document.getElementById('snackQty');
+
+        const hargaBiji = parseInt(document.getElementById('hargaBiji').textContent.replace(/[^\d]/g, ''));
+        const hargaGelas = parseInt(document.getElementById('hargaGelas').textContent.replace(/[^\d]/g, ''));
+        const hargaSnack = parseInt(document.getElementById('hargaSnack').textContent.replace(/[^\d]/g, ''));
+
+
+        const totalHargaElement = document.getElementById('totalHarga');
+        const subTotalHargaElement = document.getElementById('subTotalHarga');
+        const totalHargaMainElement = document.getElementById('totalHarga');
+
+        const diskonPersentase = 11;
+        const diskonElement = document.getElementById('diskon');
+        const diskonHargaElement = document.getElementById('diskonHarga');
+
+
+        function updateTotal() {
+            const qtyBiji = parseInt(bijiInput.value);
+            const totalHargaBiji = hargaBiji * qtyBiji;
+
+            const qtyGelas = parseInt(gelasInput.value);
+            const totalHargaGelas = hargaGelas * qtyGelas;
+
+            const qtySnack = parseInt(snackInput.value);
+
+            const totalHargaSnack = hargaSnack * qtySnack;
+
+
+            const totalHarga = totalHargaBiji + totalHargaGelas + totalHargaSnack;
+            totalHargaElement.textContent = formatRupiah(totalHarga);
+
+
+
+            const subTotal = totalHarga;
+            subTotalHargaElement.textContent = formatRupiah(subTotal);
+        }
+
+
+
+
+        function formatRupiah(angka) {
+            return new Intl.NumberFormat('id-ID', {
+                style: 'currency',
+                currency: 'IDR',
+                minimumFractionDigits: 0
+            }).format(angka).replace(/,00$/, '');
+        }
+
+        function debounce(func, wait) {
+            let timeout;
+            return function(...args) {
+                clearTimeout(timeout);
+                timeout = setTimeout(() => func.apply(this, args), wait);
+            };
+        }
+
+        const debouncedUpdateTotal = debounce(updateTotal, 100);
+
+
+
+
+        // snack
+        function kurangSnack() {
+            if (snackInput.value > 0) {
+                snackInput.value = parseInt(snackInput.value) - 1;
+                debouncedUpdateTotal();
+            } else {
+                snackInput.value = 0;
+            }
+        }
+
+        function tambahSnack() {
+            snackInput.value = parseInt(snackInput.value) + 1;
+            debouncedUpdateTotal();
+
+        }
+
+
+
+        // minuman
+        gelasInput.addEventListener('input', function() {
+            if (this.value < 0) {
+                this.value = 0;
+            }
+            debouncedUpdateTotal();
+        });
+        function kurangGelas() {
+            if (gelasInput.value > 0) {
+                gelasInput.value = parseInt(gelasInput.value) - 1;
+            } else {
+                gelasInput.value = 0;
+            }
+            debouncedUpdateTotal();
+        }
+
+        function tambahGelas() {
+            if (gelasInput.value < {{ $gelas->stok }}) {
+                gelasInput.value = parseInt(gelasInput.value) + 1;
+            }
+            debouncedUpdateTotal();
+        }
+
+
+
+        // pentol
+        const debouncedUpdateTotalPentol = debounce(updateTotal, 300);
+        function tambahBijiPentol() {
+            debouncedUpdateTotal();
+        }
+        function kurangBijiPentol(event) {
+            if (bijiInput.value >= 0) {
+                bijiInput.value = parseInt(bijiInput.value) - 1;
+            } else {
+                bijiInput.value = 0;
+            }
+            debouncedUpdateTotal();
+        }
+
+
+
+
+</script>
 @endsection
